4236f8d3e25a0801cb13f2a44a520f0e7625bd8a
Soham Soni <142389695+Soham2212004@users.noreply.github.com>
2024-11-28 20:09:54 +0530

    Add files via upload

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -19,27 +19,21 @@ import { VscAzure } from "react-icons/vsc";
 import { FaGolang } from "react-icons/fa6";
 import { GiArtificialIntelligence } from "react-icons/gi";
 
-const Technologies = () => {
-  // Define an array for the technologies and their configurations
-  const technologies = [
-    { icon: <RiReactjsLine className="text-7xl text-cyan-400" />, name: "React.js", direction: "left" },
-    { icon: <TbBrandNextjs className="text-7xl" />, name: "Next.js", direction: "right" },
-    { icon: <FaNodeJs className="text-7xl text-green-500" />, name: "Node.js", direction: "left" },
-    { icon: <SiExpress className="text-7xl text-gray-700" />, name: "Express.js", direction: "right" },
-    { icon: <SiPython className="text-7xl text-yellow-500" />, name: "Python", direction: "left" },
-    { icon: <GiArtificialIntelligence className="text-7xl text-blue-400" />, name: "AI & ML", direction: "right" },
-    { icon: <SiFirebase className="text-7xl text-yellow-600" />, name: "Firebase", direction: "left" },
-    { icon: <SiGooglecloud className="text-7xl text-blue-500" />, name: "Google Cloud", direction: "right" },
-    { icon: <VscAzure className="text-7xl text-orange-500" />, name: "Azure", direction: "left" },
-    { icon: <FaAws className="text-7xl text-orange-500" />, name: "AWS", direction: "right" },
-    { icon: <SiMongodb className="text-7xl text-green-500" />, name: "MongoDB", direction: "left" },
-    { icon: <SiDocker className="text-7xl text-blue-500" />, name: "Docker", direction: "right" },
-    { icon: <FaGolang className="text-7xl text-orange-500" />, name: "GoLang", direction: "left" },
-    { icon: <SiFlutter className="text-7xl text-blue-400" />, name: "Flutter", direction: "right" },
-    { icon: <SiGit className="text-7xl text-orange-500" />, name: "Git", direction: "left" },
-    { icon: <FaReact className="text-7xl text-blue-400" />, name: "React", direction: "right" },
-  ];
+// Icon variants for animation
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
+const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
       {/* Animated Title */}
@@ -53,23 +47,185 @@ const Technologies = () => {
       </motion.h1>
 
       {/* Icon Grid with Animation */}
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        {technologies.map((tech, index) => (
-          <motion.div
-            key={index}
-            whileInView={{ x: 0, opacity: 1 }}
-            initial={{
-              x: tech.direction === "left" ? -100 : 100,
-              opacity: 0,
-            }}
-            transition={{ duration: 1.5, delay: index * 0.1 }} // Adds a staggered animation effect
-            className="rounded-2xl border-4 border-neutral-800 p-4"
-          >
-            {tech.icon}
-            <p className="text-center mt-2">{tech.name}</p>
-          </motion.div>
-        ))}
-      </div>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        {/* React.js Icon */}
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+          <p className="text-center mt-2">React.js</p>
+        </motion.div>
+
+        {/* Next.js Icon */}
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandNextjs className="text-7xl" />
+          <p className="text-center mt-2">Next.js</p>
+        </motion.div>
+
+        {/* Node.js Icon */}
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+          <p className="text-center mt-2">Node.js</p>
+        </motion.div>
+
+        {/* Express.js Icon */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiExpress className="text-7xl text-gray-700" />
+          <p className="text-center mt-2">Express.js</p>
+        </motion.div>
+
+        {/* Python Icon */}
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPython className="text-7xl text-yellow-500" />
+          <p className="text-center mt-2">Python</p>
+        </motion.div>
+
+        {/* AI Icon */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <GiArtificialIntelligence className="text-7xl text-blue-400" />
+          <p className="text-center mt-2">AI & ML</p>
+        </motion.div>
+
+        {/* Firebase Icon */}
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiFirebase className="text-7xl text-yellow-600" />
+          <p className="text-center mt-2">Firebase</p>
+        </motion.div>
+
+        {/* Google Cloud Icon */}
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4 flex flex-col items-center justify-center" // Updated class here
+        >
+          <SiGooglecloud className="text-7xl text-blue-500" />
+          <p className="text-center mt-2">Google Cloud</p>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <VscAzure className="text-7xl text-orange-500" />
+          <p className="text-center mt-2">Azure</p>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaAws className="text-7xl text-orange-500" />
+          <p className="text-center mt-2">AWS</p>
+        </motion.div>
+
+        {/* MongoDB Icon */}
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500" />
+          <p className="text-center mt-2">MongoDB</p>
+        </motion.div>
+
+        {/* Docker Icon */}
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiDocker className="text-7xl text-blue-500" />
+          <p className="text-center mt-2">Docker</p>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaGolang className="text-7xl text-orange-500" />
+          <p className="text-center mt-2">GoLang</p>
+        </motion.div>
+
+        {/* Flutter Icon */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiFlutter className="text-7xl text-blue-400" />
+          <p className="text-center mt-2">Flutter</p>
+        </motion.div>
+
+        {/* Git Icon */}
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiGit className="text-7xl text-orange-500" />
+          <p className="text-center mt-2">Git</p>
+        </motion.div>
+
+        {/* React Icon */}
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaReact className="text-7xl text-blue-400" />
+          <p className="text-center mt-2">React</p>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
